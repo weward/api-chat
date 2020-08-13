73def0cd98c1a901eaa993a82e9222edf5fc171a
weward <dev.weward@gmail.com>
2020-08-13 09:38:22 +0800

    NC-5-api---setup-subscription-plans

---
+++ config/subscriptionplans.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    1 => [
+        'id' => 1,
+        'name' => 'default',
+        'free_user_quota' => 1,
+        'free_chat_app_quota' => 1,
+        'price_per_additional_user' => 10.00,
+        'price_per_additional_chat_app' => 2.00,
+        'unlimited_chats' => true,
+        'lack_of_agent_response_timeout' => 15, // minutes
+    ]
+];
