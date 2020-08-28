85c580fb7476e65677a91566cba8cd1a31cd64ac
Roland Edward Santos <dev.weward@gmail.com>
2020-08-28 17:04:17 +0800

    Added Chat DB Migrations

---
+++ database/migrations/2020_08_28_082322_create_chat_apps.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatApps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /**
+         * Many-to-many relationship with user_group
+         * 
+         */
+        Schema::create('chat_apps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->string('domain');
+            $table->timestamps();
+
+            $table->index(['company_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_apps');
+    }
+}
